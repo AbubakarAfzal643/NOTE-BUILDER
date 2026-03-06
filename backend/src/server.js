7282eae95709b2d4ec46e983840d8e8f9868aac8
abubakar <abubakar@gmai.com>
2026-03-06 17:42:31 +0500

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const express = require("express"); // similar to import express from 'express'
 const cors = require('cors')
 const dotenv = require("dotenv").config();
+const path = require("path")
 const app = express();
 const connectDB = require("./config/db");
 const notesRoutes = require("./routes/notesRoutes");
@@ -8,9 +9,11 @@ const rateLimiter = require("./middleware/rateLimiter");
 const PORT = process.env.PORT || 5001;
 
 
-app.use(cors({
-  origin: /^http:\/\/localhost:\d+$/,
-}));
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors({
+    origin: /^http:\/\/localhost:\d+$/,
+  }));
+} 
 app.use(express.json());
 app.use(rateLimiter);
 
@@ -23,6 +26,14 @@ app.use((req, res, next) => {
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/notes", notesRoutes);
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+app.get("/{*path}",(req,res) =>{
+  res.sendFile(path.join(__dirname, "../frontend" , "dist" , "index.html"))
+})
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
